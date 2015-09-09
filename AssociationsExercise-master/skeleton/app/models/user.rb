9ebe1f0ec3663bc5b255dfe380da59843be6443e
Christopher Huang <cchuang90@gmail.com>
2015-09-09 12:29:36 -0700

    First Commit

---
+++ AssociationsExercise-master/skeleton/app/models/user.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  has_many :enrollments,
+    class_name: :Enrollment,
+    foreign_key: :student_id,
+    primary_key: :id
+
+  has_many :course_enrollments, class_name: :Course, through: :enrollments
+
+  has_many :courses,
+    class_name: :Course,
+    foreign_key: :instructor_id,
+    primary_key: :id
+end
