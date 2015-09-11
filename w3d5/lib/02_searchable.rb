dd75cfd524a1d7f9e7330a44bff489800c49ba8b
Christopher Huang <CCHuang90@gmail.com>
2015-09-11 15:17:28 -0700

    finished has_many and belongs_to

--- w3d5/lib/02_searchable.rb
+++ w3d5/lib/02_searchable.rb
@@ -3,10 +3,28 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    p params
+    if params.is_a?(Hash)
+      columns = []
+      values = []
+      params.each do |key, value|
+        columns << key.to_s + " = ?"
+        values << value
+      end
+    end
+      records = DBConnection.execute(<<-SQL, values)
+        SELECT
+          *
+        FROM
+          #{self.table_name}
+        WHERE
+          #{columns.join(" AND ")}
+      SQL
+      self.parse_all(records)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
--- w3d5/lib/03_associatable.rb
+++ w3d5/lib/03_associatable.rb
@@ -10,34 +10,53 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || (name.to_s + "_id").to_sym
+    @class_name = options[:class_name] || name.to_s.capitalize
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || (self_class_name.downcase + "_id").to_sym
+    @class_name = options[:class_name] || name.to_s.capitalize[0...-1]
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      f_key = self.send(options.foreign_key)
+      table = options.table_name
+      p_key = options.primary_key
+
+      options.model_class.where(p_key => f_key).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    # debugger
+    options = HasManyOptions.new(name, self.to_s, options)
+    define_method(name) do
+      f_key = options.foreign_key
+      table = options.table_name
+      p_key = self.send(options.primary_key)
+
+      options.model_class.where(f_key => p_key)
+    end
   end
 
   def assoc_options
@@ -47,4 +66,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
